1341d980490a2616426beb7c46be4f0c87b3173e
zhangdongjava <787395919@qq.com>
2017-04-20 17:39:32 +0800

    1

--- test1/src/com/zzz/concurrent/Test1.java
+++ test1/src/com/zzz/concurrent/Test1.java
@@ -1,22 +1,27 @@
 package com.zzz.concurrent;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
 /**
  * Created by dell_2 on 2016/8/23.
  */
 public class Test1 {
-    public static void main(String[] args) {
-        new Thread(()->{
-            for (int i = 0; i < 10; i++) {
-                System.out.println(i);
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService threadPool = Executors.newCachedThreadPool();
+        Future future = threadPool.submit(() -> {
+            List<String> list = new ArrayList<>(10);
+            list.add("111");
+            list.add("222");
+            list.add("333");
+            list.stream().forEach(System.out::println);
+            return list.size();
+        });
 
-            }
-        }).start();
+        threadPool.shutdown();
+        System.out.println(future.get());
     }
-
-
-
-
-
 
 
 }
